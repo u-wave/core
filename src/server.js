f5f0c69117dd67a176f08ab616363c8a46171dba
SooYou <souyoou@googlemail.com>
2015-09-15 04:23:02 +0200

    refactor server
    before this rewrite, the server was pretty much just a test app to make
    sure the database and routes work correctly, this major rewrite makes it
    much easier to interact with the server.

--- src/server.js
+++ src/server.js
@@ -1,53 +1,80 @@
 import bodyParser from 'body-parser';
+import EventEmitter from 'events';
 import mongoose from 'mongoose';
 import readline from 'readline';
 import express from 'express';
 import redis from 'redis';
 import debug from 'debug';
 
-// APIs
-import v1 from './routes/v1/api.js';
+export default class UWaveServer extends EventEmitter {
+  constructor(config = {}) {
+    super();
+    this.config = config;
+    this.app = express();
+    this.mongo = mongoose.connection;
 
-// config
-import serverConfig from './config/server.json';
-import mongoConfig from './config/mongo.json';
-import apiConfig from './config/api.json';
+    this.log = debug('uwave:server');
+    this.mongoLog = debug('uwave:mongo');
 
-const app = express();
-const mongo = mongoose.connection;
+    this.started = false;
 
-const serverLog = debug("uwave:server");
-const mongoLog = debug("uwave:database:mongo");
+    /* ======== mongo events ======== */
+    this.mongo.on('error', e => {
+      this.mongoLog(e);
+      this.emit('mongoError', e);
+    });
 
-mongo.on('error', e => mongoLog(e));
-mongo.on('reconnected', () => mongoLog("reconnected"));
-mongo.on('disconnected', () => mongoLog("disconnected"))
+    this.mongo.on('reconnected', () => {
+      this.mongoLog('reconnected');
+      this.emit('mongoReconnect');
+    });
 
-mongo.once('open', () => {
-  // Initialize app now that we have a connection to the database
-  mongoLog('connection successfull');
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+    this.mongo.on('disconnected', () => {
+      this.mongoLog('disconnected');
+      this.emit('mongoDisconnect');
+    });
 
-  // here you can invoke further API versions
-  v1(app, apiConfig.v1);
-});
+    /* ======== SIGINT ======== */
+    // workaround to properly stop the server on termination.
+    if (process.platform === "win32") {
+      readline.createInterface({
+        'input': process.stdin,
+        'output': process.stdout
+      }).on('SIGINT', () => process.emit('SIGINT'));
+    }
 
+    process.on('SIGINT', () => { this.stop(); });
+  }
 
-// workaround to properly close all connections on termination.
-if(process.platform === "win32") {
-  readline.createInterface({
-    'input': process.stdin,
-    'output': process.stdout
-  }).on('SIGINT', () => process.emit('SIGINT'));
+  registerAPI(path, router) {
+    if (typeof path !== "string") throw new Error("path has to be of type string");
+    if (typeof router instanceof express.Router) throw new Error("path has to be an instance of router");
+
+    this.app.use(path, router);
+  }
+
+  start() {
+    this.log('starting server...');
+
+    this.mongo.once('open', () => {
+      this.mongoLog('connection successful');
+
+      this.app.use(bodyParser.json());
+      this.app.use(bodyParser.urlencoded({ extended: true }));
+      this.app.listen(this.config.server.port);
+
+      this.emit('started');
+    });
+
+    mongoose.connect(`mongodb://${this.config.mongo.host}/uwave:${this.config.mongo.port}`, this.config.mongo.options);
+  }
+
+  stop() {
+    this.log('stopping server...');
+
+    mongoose.connection.close(() => {
+      this.mongoLog('connection closed');
+      this.emit('stopped');
+    });
+  }
 }
-
-process.on('SIGINT', () => {
-  mongoose.connection.close(() => {
-    mongoLog('closing connection to mongo');
-    process.exit(0);
-  });
-});
-
-mongoLog(`connecting to database '${mongoConfig.host}/uwave'...`);
-mongoose.connect(`mongodb://${mongoConfig.host}/uwave:${mongoConfig.port}`, mongoConfig.options);
