7d1745358227831d4e96b6f5205024454eeaa567
SooYou <souyoou@googlemail.com>
2015-09-15 14:08:59 +0200

    extend server example with middleware

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
 import UWaveServer from './server';
-import v1 from "./apis/v1/api";
+
+// middleware
+import rateLimiter from './middleware/ratelimiter';
+import permissions from './middleware/permissions';
+import authenticator from './middleware/authenticator';
+
+// API
+import v1 from './apis/v1/api';
+
+// config
 import config from './config/uwave';
 
 const server = new UWaveServer(config);
+const middleware = [
+  rateLimiter,
+  authenticator,
+  permissions
+];
 
 server.on('stopped', () => {
   process.exit(0);
 });
 
-server.registerMiddleware()
-server.registerAPI('/v1', v1());
+server.registerAPI('/v1', v1(middleware));
 
 server.start();
