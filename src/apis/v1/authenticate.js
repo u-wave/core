3a42d296040ebc705d7c3f37b7cd7992394bb687
SooYou <souyoou@googlemail.com>
2015-09-16 05:35:36 +0200

    add working test example for register

--- src/apis/v1/authenticate.js
+++ src/apis/v1/authenticate.js
@@ -3,58 +3,60 @@ import crypto from 'crypto';
 import debug from 'debug';
 
 import User from '../../models/user';
+import Authentication from '../../models/authentication';
 
-let authLog = debug("v1/auth");
+const authLog = debug('api:v1:auth');
 
 export default function authenticate(router) {
   router.post('/auth/register', (req, res) => {
-    let errors = [];
+    const errors = [];
 
-    if(!req.body.email) errors.push('no email set');
-    if(!req.body.username) errors.push('no username set');
-    if(!req.body.password) errors.push('no password set');
-    if(!req.body.passwordRepeat) errors.push('no password confirmation set');
+    if (!req.body.email) errors.push('no email set');
+    if (!req.body.username) errors.push('no username set');
+    if (!req.body.password) errors.push('no password set');
+    if (!req.body.passwordRepeat) errors.push('no password confirmation set');
 
-    if(errors.length > 0) return res.status(403).json({ 'error': errors });
+    if (errors.length > 0) return res.status(403).json(errors);
 
-    if(req.body.password !== req.body.passwordRepeat) return res.status(422).json({ 'error': 'passwords don\'t match' });
+    if (req.body.password !== req.body.passwordRepeat) return res.status(422).json('passwords don\'t match');
 
     authLog(`creating new user ${req.body.email}`);
 
-    let user = new User({ 'username': req.body.username });
+    const user = new User({ 'username': req.body.username });
 
     user.save(e => {
-      if(e) return res.status(422).json({ 'error': e });
+      if (e) return res.status(422).json(e);
 
       authLog(`user ${user.username} created`);
       crypto.randomBytes(256, (e, buf) => {
-        if(e) {
+        if (e) {
           authLog(`couldn't create random byte buffer. Err: ${e}`);
-          return res.status(500).json({ 'error': 'could not create user, please try again later' });
+          return res.status(500).json('could not create user, please try again later');
         }
-        let salt = buf.toString('hex');
+        const salt = buf.toString('hex');
 
-        crypto.pbkdf2(req.body.password, salt, 2048, 'sha256', (e, hash) => {
-          if(e) {
+        crypto.pbkdf2(req.body.password, salt, 2048, 256, 'sha256', (e, hash) => {
+          if (e) {
             authLog(`couldn't salt password. Err: ${e}`);
-            return res.status(500).json({ 'error': 'could not set password, please try again later' });
+            return res.status(500).json('could not set password, please try again later');
           }
 
-          let auth = new Authentication({
+          const auth = new Authentication({
             'user': user.id,
             'email': req.body.email,
-            'hash': hash,
+            'hash': hash.toString('hex'),
             'salt': salt
           });
 
           auth.save(e => {
-            if(e) return res.status(422).json({ 'error': e });
+            if (e) return res.status(422).json({ 'error': e });
 
             authLog(`user ${user.username} successfully saved!`);
+            return res.status(200).send(user.toJSON());
           });
         });
       });
-    })
+    });
   });
 
   router.post('/auth/login', (req, res) => {
