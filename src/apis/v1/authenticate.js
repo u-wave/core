2b40740ea84f0cba1436f0f211152fe66a701db9
SooYou <souyoou@googlemail.com>
2015-09-17 17:49:21 +0200

    refactor auth/register to use a promise chain

--- src/apis/v1/authenticate.js
+++ src/apis/v1/authenticate.js
@@ -1,15 +1,19 @@
 import jwt from 'jsonwebtoken';
+import bluebird from 'bluebird';
 import crypto from 'crypto';
 import debug from 'debug';
 
 import User from '../../models/user';
 import Authentication from '../../models/authentication';
 
-const authLog = debug('api:v1:auth');
+const log = debug('api:v1:auth');
+const pbkdf2 = bluebird.promisify(crypto.pbkdf2);
+const randomBytes = bluebird.promisify(crypto.randomBytes);
 
 export default function authenticate(router) {
   router.post('/auth/register', (req, res) => {
     const errors = [];
+    let salt = null;
 
     if (!req.body.email) errors.push('no email set');
     if (!req.body.username) errors.push('no username set');
@@ -20,42 +24,40 @@ export default function authenticate(router) {
 
     if (req.body.password !== req.body.passwordRepeat) return res.status(422).json('passwords don\'t match');
 
-    authLog(`creating new user ${req.body.email}`);
+    log(`creating new user ${req.body.email}`);
 
     const user = new User({ 'username': req.body.username });
 
-    user.save(e => {
-      if (e) return res.status(422).json(e);
-
-      authLog(`user ${user.username} created`);
-      crypto.randomBytes(256, (e, buf) => {
-        if (e) {
-          authLog(`couldn't create random byte buffer. Err: ${e}`);
-          return res.status(500).json('could not create user, please try again later');
-        }
-        const salt = buf.toString('hex');
-
-        crypto.pbkdf2(req.body.password, salt, 2048, 256, 'sha256', (e, hash) => {
-          if (e) {
-            authLog(`couldn't salt password. Err: ${e}`);
-            return res.status(500).json('could not set password, please try again later');
-          }
-
-          const auth = new Authentication({
-            'user': user.id,
-            'email': req.body.email,
-            'hash': hash.toString('hex'),
-            'salt': salt
-          });
-
-          auth.save(e => {
-            if (e) return res.status(422).json({ 'error': e });
-
-            authLog(`user ${user.username} successfully saved!`);
-            return res.status(200).send(user.toJSON());
-          });
-        });
-      });
+    user.save()
+    // generate salt
+    .then(() => {
+      log(`user ${user.username} successfully validated`);
+      return randomBytes(256);
+    })
+    // encrypt password
+    .then(buf => {
+      salt = buf.toString('hex');
+      return pbkdf2(req.body.password, salt.toString('hex'), 2048, 256, 'sha256');
+    })
+    // create authentication entry
+    .then(hash => {
+      log(`hash created successfully, setting up auth entry...`);
+      return new Authentication({
+        'user': user.id,
+        'email': req.body.email,
+        'hash': hash.toString('hex'),
+        'salt': salt
+      })
+      .save();
+    })
+    // send out the data
+    .then(() => {
+      log(`user ${user.username} created successfully`);
+      res.status(200).json(user.toJSON());
+    })
+    .catch(e => {
+      log(`couldn't register user ${user.username}. Err: ${e}`);
+      res.status(500).json('internal server error, please try again later');
     });
   });
 
