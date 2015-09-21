626b31752ea4f1a03323f94e278f852a41c217ff
SooYou <souyoou@googlemail.com>
2015-09-21 03:47:16 +0200

    add basic checks for role

--- src/apis/v1/booth.js
+++ src/apis/v1/booth.js
@@ -1,10 +1,10 @@
 export default function booth(router) {
   router.post('/booth/skip', (req, res) => {
-
+    if (!req.user || req.user.role < 3) return res.status(412).json('you need to be at least bouncer to do this');
   });
 
   router.post('/booth/replace', (req, res) => {
-
+    if (!req.user || req.user.role < 3) return res.status(412).json('you need to be at least bouncer to do this');
   });
 
   router.post('/booth/favorite', (req, res) => {
