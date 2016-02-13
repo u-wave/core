d776c49f2d0bf2efc7fc8f8c2efd883f359b46ad
René Kooi <rene@kooi.me>
2016-02-13 18:37:19 +0100

    test: add Media Source API tests

--- test/server.js
+++ test/server.js
@@ -4,7 +4,8 @@ import Server from '../lib/server';
 
 describe('üWave Server', () => {
   it('can instantiate a Server object', () => {
-    expect(new Server({ /* options */ }))
-      .to.be.instanceOf(Server);
+    expect(
+      new Server(require('./test-server-config'))
+    ).to.be.instanceOf(Server);
   });
 });
