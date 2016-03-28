c8007c7d9e4ed0e33e50617bc90d0bc407a47cd9
René Kooi <rene@kooi.me>
2016-03-28 17:05:17 +0200

    models: add indices to commonly searched for fields

--- src/models/Media.js
+++ src/models/Media.js
@@ -3,8 +3,8 @@ import { createSchema } from 'mongoose-model-decorators';
 export default () => {
   class Media {
     static schema = {
-      sourceID: { type: String, max: 128, required: true },
-      sourceType: { type: String, max: 128, required: true },
+      sourceID: { type: String, max: 128, required: true, index: true },
+      sourceType: { type: String, max: 128, required: true, index: true },
       sourceData: {},
       artist: { type: String, max: 128, required: true },
       title: { type: String, max: 128, required: true },
--- src/models/User.js
+++ src/models/User.js
@@ -4,9 +4,9 @@ export default uw => {
   class User {
     static schema = {
       joined: { type: Date, default: Date.now },
-      username: { type: String, min: 3, max: 32, required: true, unique: true },
+      username: { type: String, min: 3, max: 32, required: true, unique: true, index: true },
       language: { type: String, min: 2, max: 2, default: 'en' },
-      role: { type: Number, min: 0, max: 5, default: 0 },
+      role: { type: Number, min: 0, max: 5, default: 0, index: true },
       avatar: { type: String, min: 0, max: 256, default: '' },
       slug: { type: String, min: 3, max: 256, required: true },
       level: { type: Number, min: 0, max: 9001, default: 0 },
