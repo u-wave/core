94100ee40bca342587b8835fc2e457bdab0cfe59
Renée Kooi <renee@kooi.me>
2017-07-09 12:40:57 +0200

    Use temporary databases instead of mockgoose while running tests, closes #91 (#123)
    * test: Use temp databases instead of mockgoose, closes #91
    
    * ci: Add mongoDB to services.

--- test/init.js
+++ test/init.js
@@ -2,8 +2,6 @@ import Bluebird from 'bluebird';
 import chai from 'chai';
 import asPromised from 'chai-as-promised';
 import sinonChai from 'sinon-chai';
-import { Mockgoose } from 'mockgoose';
-import mongoose from 'mongoose';
 
 Bluebird.config({
   longStackTraces: true,
@@ -12,11 +10,3 @@ Bluebird.config({
 
 chai.use(asPromised);
 chai.use(sinonChai);
-
-const mockgoose = new Mockgoose(mongoose);
-before(async function prepareMockgoose() {
-  // May have to download mongodb-prebuilt which can take a long time.
-  this.timeout(0);
-  mockgoose.helper.setDbVersion('3.4.3');
-  await mockgoose.prepareStorage();
-});
