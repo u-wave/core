2a20ce8aaec79a1c228647fc8918d8f2af80860b
Renée Kooi <renee@kooi.me>
2017-10-07 17:37:43 +0200

    Add missing file

---
+++ test/utils/mongoConnected.js
@@ -0,0 +1,8 @@
+export default function isMongoConnected(mongo) {
+  if (mongo.readyState === 1) {
+    return Promise.resolve();
+  }
+  return new Promise((resolve) => {
+    mongo.once('open', resolve);
+  });
+}
