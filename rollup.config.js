b27cf2f9b3bd1b95da48f686766f572442b86381
Renée Kooi <renee@kooi.me>
2018-01-13 13:58:17 +0100

    build: generate cjs and es modules builds (#166)

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,24 @@ import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import isBuiltinModule from 'is-builtin-module';
 
-const { name, dependencies } = require('./package.json');
+const pkg = require('./package.json');
 
-const external = Object.keys(dependencies);
+const external = Object.keys(pkg.dependencies);
 
 process.env.BABEL_ENV = 'rollup';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: `dist/${name}.js`,
+  output: [{
+    file: pkg.main,
+    exports: 'default',
     format: 'cjs',
     sourcemap: true,
-  },
+  }, {
+    file: pkg.module,
+    format: 'es',
+    sourcemap: true,
+  }],
   external: id => isBuiltinModule(id) || external.some(m => id.split('/')[0] === m),
   plugins: [
     babel(),
