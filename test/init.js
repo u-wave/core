d9c2de899379cf073aed30f4db03ad04b05241c2
Renée Kooi <renee@kooi.me>
2018-02-18 12:52:22 +0100

    remove last bluebird

--- test/init.js
+++ test/init.js
@@ -1,12 +1,6 @@
-import Bluebird from 'bluebird';
 import chai from 'chai';
 import asPromised from 'chai-as-promised';
 import sinonChai from 'sinon-chai';
-
-Bluebird.config({
-  longStackTraces: true,
-  warnings: true,
-});
 
 chai.use(asPromised);
 chai.use(sinonChai);
