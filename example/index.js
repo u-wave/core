fd6d3e007d75378b33b715f67646a47367bdda4d
Renée Kooi <renee@kooi.me>
2018-02-22 19:29:07 +0100

    some example fixes

--- example/index.js
+++ example/index.js
@@ -1,12 +1,10 @@
-import express from 'express';
-import { Buffer } from 'buffer';
-
-import uwave from 'u-wave-core';
-import createWebApi from 'u-wave-api-v1';
-import createWebClient from 'u-wave-web';
-
-import youTubeSource from 'u-wave-source-youtube';
-import soundCloudSource from 'u-wave-source-soundcloud';
+const express = require('express');
+const { Buffer } = require('buffer');
+const uwave = require('u-wave-core');
+const createWebApi = require('u-wave-http-api');
+const createWebClient = require('u-wave-web/middleware').default;
+const youTubeSource = require('u-wave-source-youtube');
+const soundCloudSource = require('u-wave-source-soundcloud');
 
 const port = process.env.PORT || 80;
 
@@ -16,19 +14,19 @@ const uw = uwave(config);
 
 // Register your Media Sources. The API keys are configured in the config.json
 // file.
-uw.source('youtube', youTubeSource, config.youtube);
-uw.source('soundcloud', soundCloudSource, config.soundcloud);
+uw.source(youTubeSource, config.youtube);
+uw.source(soundCloudSource, config.soundcloud);
 
 const app = express();
 const server = app.listen(port);
 
-app.use('/v1', createWebApi(uw, {
-  secret: new Buffer(config.secret),
+app.use('/api', createWebApi(uw, {
+  secret: Buffer.from(config.secret),
   // The web API needs an HTTP server to attach the WebSocket server to.
   server: server
 }));
 app.use(createWebClient(uw, {
-  apiBase: '/v1'
+  apiBase: '/api'
 }));
 
 uw.on('started', () => {
