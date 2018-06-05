542d79d0c1ca5681939dc50aa22fd110871caa1d
Renée Kooi <renee@kooi.me>
2018-06-05 15:31:05 +0200

    Move waitlist into core
    I tried this a bunch of times, integrating the waitlist nicely with the
    existing booth code, but always got stuck. So instead, this is a simple
    copy-paste job from http-api, which is at least a start. This makes it
    easier to apply small changes in the future to work towards a nicer
    integration.

---
+++ src/errors/PermissionError.js
@@ -0,0 +1,12 @@
+import UwaveError from './UwaveError';
+
+export default class PermissionError extends UwaveError {
+  public = true;
+  name = 'PermissionError';
+  code = 'NOT_ALLOWED';
+
+  constructor (message, { requiredRole }) {
+    super(message);
+    this.requiredRole = requiredRole;
+  }
+}
