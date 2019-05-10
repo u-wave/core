f8efd633f24c7e36a8eed9721c168629f633d22b
Renée Kooi <renee@kooi.me>
2019-05-10 12:05:56 +0200

    Remove unnecessary resolution hack from eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,3 @@
-const Module = require('module');
-const { types, template, traverse } = require('@babel/core');
-
-// Ensure babel-eslint uses our current Babel version.
-// This is ABSOLUTELY TERRIBLE and should be removed ASAP when
-// babel-eslint releases a new version.
-const req = Module.prototype.require;
-Module.prototype.require = function (id) {
-  if (id === '@babel/types') return types;
-  if (id === '@babel/template') return { default: template };
-  if (id === '@babel/traverse') return { default: traverse };
-  return req.call(this, id);
-};
-
 module.exports = {
   extends: 'airbnb-base',
   parser: 'babel-eslint',
