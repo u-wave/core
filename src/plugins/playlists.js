u-wave/core

commit 3922491c1d5bc387049cd49a1510e1d7f7f5494a

playlists: faster getPlaylistItems with a single query (#351)
* playlists: use a single aggregation query to fetch playlist items

Previously, pagination was done in JavaScript and we needed separate
methods to do filtering.

MongoDB has a `$lookup` aggregation stage to join data from a different
collection. We can use it to associate playlist items with the playlist
on the DB side, and later to get the media object for each playlist
item.

MongoDB has a `$facet` aggregation stage that allows you to split up the
stages: one to only count _all_ filter matches, and one to return the
playlist items that match the filter and survived pagination.

* Add an index to `PlaylistItem#media`.

* Use pre-mongo-4.2 way to replace root

src/plugins/playlists.js
@@ -119,27 +119,6 @@ export class PlaylistsRepository {
     return {};
   }
 
-  async getPlaylistItemIDsFiltered(playlist, filter) {
-    const PlaylistItem = this.uw.model('PlaylistItem');
-    const rx = new RegExp(escapeStringRegExp(filter), 'i');
-    const matches = await PlaylistItem.where({
-      _id: { $in: playlist.media },
-      $or: [{ artist: rx }, { title: rx }],
-    }).select('_id');
-
-    const allItemIDs = matches.map((item) => item.id);
-
-    // We want this sorted by the original playlist item order, so we can
-    // just walk through the original playlist and only keep the items that we
-    // need.
-    return playlist.media.filter((id) => allItemIDs.indexOf(`${id}`) !== -1);
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  async getPlaylistItemIDsUnfiltered(playlist) {
-    return playlist.media;
-  }
-
   async getPlaylistItem(itemID) {
     const PlaylistItem = this.uw.model('PlaylistItem');
 
@@ -162,29 +141,69 @@ export class PlaylistsRepository {
   }
 
   async getPlaylistItems(playlistOrID, filter = null, pagination = null) {
-    const PlaylistItem = this.uw.model('PlaylistItem');
     const playlist = await this.getPlaylist(playlistOrID);
-    const filteredItemIDs = filter
-      ? await this.getPlaylistItemIDsFiltered(playlist, filter)
-      : await this.getPlaylistItemIDsUnfiltered(playlist);
 
-    let itemIDs = filteredItemIDs;
-    if (pagination) {
-      const start = pagination.offset;
-      const end = start + pagination.limit;
-      itemIDs = itemIDs.slice(start, end);
+    const aggregate = [
+      // find the playlist
+      { $match: { _id: playlist._id } },
+      { $limit: 1 },
+      // find the items
+      {
+        $lookup: {
+          from: 'playlistitems', localField: 'media', foreignField: '_id', as: 'items',
+        },
+      },
+      { $project: { _id: 0, items: 1 } },
+      { $unwind: '$items' },
+      { $replaceRoot: { newRoot: '$items' } },
+    ];
+
+    if (filter) {
+      const rx = new RegExp(escapeStringRegExp(filter), 'i');
+      aggregate.push({
+        $match: {
+          $or: [{ artist: rx }, { title: rx }],
+        },
+      });
     }
-    const items = itemIDs.length > 0
-      ? await PlaylistItem.find()
-        .where('_id').in(itemIDs)
-        .populate('media')
-      : [];
 
-    const results = itemIDs.map((itemID) => items.find((item) => `${item.id}` === `${itemID}`));
+    const aggregateCount = [
+      { $count: 'filtered' },
+    ];
+    const aggregateItems = [];
 
-    return new Page(results, {
+    if (pagination) {
+      aggregateItems.push(
+        { $skip: pagination.offset },
+        { $limit: pagination.limit },
+      );
+    }
+
+    // look up the media items after this is all filtered down
+    aggregateItems.push(
+      {
+        $lookup: {
+          from: 'media', localField: 'media', foreignField: '_id', as: 'media',
+        },
+      },
+      { $unwind: '$media' }, // is always 1 item, is there a better way than $unwind?
+    );
+
+    aggregate.push({
+      $facet: {
+        count: aggregateCount,
+        items: aggregateItems,
+      },
+    });
+
+    const [{ count, items }] = await this.uw.model('Playlist').aggregate(aggregate);
+
+    // `items` is the same shape as a PlaylistItem instance!
+
+    return new Page(items, {
       pageSize: pagination ? pagination.limit : null,
-      filtered: filteredItemIDs.length,
+      // `count` can be the empty array if the playlist has no items
+      filtered: count.length === 1 ? count[0].filtered : playlist.media.length,
       total: playlist.media.length,
 
       current: pagination,

src/models/PlaylistItem.js
@@ -5,7 +5,12 @@ const { Types } = mongoose.Schema;
 
 export default function playlistItemModel() {
   const schema = new Schema({
-    media: { type: Types.ObjectId, ref: 'Media', required: true },
+    media: {
+      type: Types.ObjectId,
+      ref: 'Media',
+      required: true,
+      index: true,
+    },
     artist: {
       type: String,
       max: 128,

commit d6c70d6916b1c83aec30e7da90fd7ab37463eb14

Respect playlist item order in `getPlaylistItems`
This pretty important feature was accidentally broken in #351.

It would do the playlist items lookup in one go for the entire playlist,
but then order is not guaranteed. This changes it to do the lookup for
individual playlist items. Hopefully it doesn't tank performance too
much :)

src/plugins/playlists.js
@@ -148,14 +148,16 @@ class PlaylistsRepository {
       { $match: { _id: playlist._id } },
       { $limit: 1 },
       // find the items
+      { $project: { _id: 0, media: 1 } },
+      { $unwind: '$media' },
       {
         $lookup: {
-          from: 'playlistitems', localField: 'media', foreignField: '_id', as: 'items',
+          from: 'playlistitems', localField: 'media', foreignField: '_id', as: 'item',
         },
       },
-      { $project: { _id: 0, items: 1 } },
-      { $unwind: '$items' },
-      { $replaceRoot: { newRoot: '$items' } },
+      // return only the items
+      { $unwind: '$item' }, // just one each
+      { $replaceRoot: { newRoot: '$item' } },
     ];
 
     if (filter) {