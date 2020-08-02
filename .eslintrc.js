3fb8b9a7bd947ee76875a4c184d67bf17144c3d8
Renée Kooi <renee@kooi.me>
2020-08-02 18:43:57 +0200

    Remove p-each-series (#392)
    It just does a `for..of` + `await` anyways.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,21 @@ module.exports = {
     'no-param-reassign': ['error', { props: false }],
     // I disagree that this is bad
     'max-classes-per-file': 'off',
+    // Allow `for..of`
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
   },
 };
