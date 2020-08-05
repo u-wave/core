75a669afe83c33618d308d1927dc6dfcd46c05e2
Renée Kooi <renee@kooi.me>
2020-08-05 14:09:00 +0200

    auth: move social auth schema to a file

--- src/plugins/passport.js
+++ src/plugins/passport.js
@@ -7,48 +7,7 @@ const JWTStrategy = require('../auth/JWTStrategy');
 
 const AUTH_SETTINGS_KEY = 'u-wave:socialAuth';
 
-const schema = {
-  type: 'object',
-  title: 'Social Login',
-  description: 'Settings for third party login integrations like Google.',
-  properties: {
-    google: {
-      type: 'object',
-      title: 'Google Authentication',
-      description: 'Settings for the Google authentication integration.',
-      properties: {
-        enabled: {
-          type: 'boolean',
-          title: 'Enabled',
-          default: false,
-        },
-        clientID: {
-          type: 'string',
-          title: 'Client ID',
-          description: 'The OAuth2 Client ID.',
-        },
-        clientSecret: {
-          type: 'string',
-          title: 'Client Secret',
-          description: 'The secret key.',
-        },
-        callbackURL: {
-          type: 'string',
-          format: 'uri-reference',
-          title: 'Callback URL',
-          description: 'The URL that Google will redirect to once a signin is complete. This URL should contain the code that finalizes the login on the üWave end.',
-        },
-      },
-      required: ['enabled'],
-      // When enabled, clientID and clientSecret are required.
-      dependencies: {
-        enabled: ['clientID', 'clientSecret'],
-      },
-      default: {},
-    },
-  },
-  required: ['google'],
-};
+const schema = require('../schemas/socialAuth.json');
 
 class PassportPlugin extends Passport {
   constructor(uw, options) {
@@ -78,14 +37,14 @@ class PassportPlugin extends Passport {
     }, callbackify(localLogin)));
     this.use('jwt', new JWTStrategy(options.secret, (user) => uw.users.getUser(user.id)));
 
-    uw.config.register(AUTH_SETTINGS_KEY, schema);
+    uw.config.register(schema['uw:key'], schema);
     uw.config.on('set', (key, settings) => {
       if (key === AUTH_SETTINGS_KEY) {
         this.applyAuthStrategies(settings);
       }
     });
 
-    uw.config.get(AUTH_SETTINGS_KEY)
+    uw.config.get(schema['uw:key'])
       .then((settings) => this.applyAuthStrategies(settings))
       .catch((err) => {
         debug('social auth setup error', err);
