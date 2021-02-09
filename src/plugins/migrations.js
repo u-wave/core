f2fab53c2ea49c50a8a7b45ebe124fe5d7b5714c
Renée Kooi <renee@kooi.me>
2021-02-09 19:01:39 +0100

    use umzug

--- src/plugins/migrations.js
+++ src/plugins/migrations.js
@@ -1,72 +1,38 @@
 const path = require('path');
-const { promisify } = require('util');
-const mongoose = require('mongoose');
 const RedLock = require('redlock');
-const migrate = require('migrate');
+const { Umzug, MongoDBStorage } = require('umzug');
 const debug = require('debug')('uwave:migrate');
-
-const { Schema } = mongoose;
-
-const schema = new Schema({}, {
-  collection: 'migrations',
-  timestamps: true,
-});
-
-class MongoStore {
-  constructor(db) {
-    this.db = db;
-    this.Migration = this.db.model('Migration', schema);
-  }
-
-  load(callback) {
-    this.Migration.find().sort({ createdAt: -1 }).findOne((err, set) => {
-      if (err) {
-        callback(err);
-      } else if (set) {
-        callback(null, set);
-      } else {
-        callback(null, {});
-      }
-    });
-  }
-
-  save(set, callback) {
-    this.Migration.create(set, (err) => {
-      callback(err);
-    });
-  }
-}
 
 async function migrationsPlugin(uw) {
   const redLock = new RedLock([uw.redis]);
 
-  const lock = await redLock.lock('migrate', 10000);
-  try {
-    const store = new MongoStore(uw.mongo);
-    const load = promisify(migrate.load);
-
-    const set = await load({
-      stateStore: store,
-      migrationsDirectory: path.join(__dirname, '../migrations'),
+  async function migrate(migrations) {
+    const migrator = new Umzug({
+      migrations,
+      context: uw,
+      storage: new MongoDBStorage({
+        collection: uw.mongo.collection('migrations'),
+      }),
+      logger: {
+        debug: debug.extend('debug'),
+        info: debug.extend('info'),
+        warn: debug.extend('warn'),
+        error: debug.extend('error'),
+      },
     });
 
-    set.on('migration', (migration, direction) => {
-      debug('applying migration', migration.title, direction);
-    });
-
-    set.migrations.forEach((migration) => {
-      if (migration.up) {
-        migration.up = migration.up.bind(migration, uw);
-      }
-      if (migration.down) {
-        migration.down = migration.down.bind(migration, uw);
-      }
-    });
-
-    await promisify(set.up)();
-  } finally {
-    await lock.unlock();
+    const lock = await redLock.lock('migrate', 10000);
+    try {
+      await migrator.up();
+    } finally {
+      await lock.unlock();
+    }
   }
+  uw.migrate = migrate;
+
+  await uw.migrate({
+    glob: ['*.js', { cwd: path.join(__dirname, '../migrations') }],
+  });
 }
 
 module.exports = migrationsPlugin;
