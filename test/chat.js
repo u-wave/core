c09160aca13a20ada102421abb0d6593d5bc5f33
Renée Kooi <renee@kooi.me>
2021-02-09 18:39:00 +0100

    delay 500 again

--- test/chat.js
+++ test/chat.js
@@ -49,7 +49,7 @@ describe('Chat', () => {
     ws.send(JSON.stringify({ command: 'sendChat', data: 'unmuted' }));
     mutedWs.send(JSON.stringify({ command: 'sendChat', data: 'muted' }));
 
-    await delay(200);
+    await delay(500);
 
     console.log(spy.getCalls().map((call) => call.args));
 
