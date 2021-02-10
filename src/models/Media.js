9cb81a5e228017637b6bde301e5b81f674b589a5
Renée Kooi <renee@kooi.me>
2021-02-10 13:25:17 +0100

    Install all models in one avvio plugin (#420)

--- src/models/Media.js
+++ src/models/Media.js
@@ -4,35 +4,31 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-async function mediaModel(uw) {
-  const schema = new Schema({
-    sourceID: {
-      type: String, max: 128, required: true, index: true,
-    },
-    sourceType: {
-      type: String, max: 128, required: true, index: true,
-    },
-    sourceData: {},
-    artist: {
-      type: String,
-      max: 128,
-      required: true,
-      set: (artist) => artist.normalize('NFKC'),
-    },
-    title: {
-      type: String,
-      max: 128,
-      required: true,
-      set: (title) => title.normalize('NFKC'),
-    },
-    duration: { type: Number, min: 0, default: 0 },
-    thumbnail: { type: String, max: 256, default: '' },
-  }, {
-    timestamps: true,
-    minimize: false,
-  });
+const schema = new Schema({
+  sourceID: {
+    type: String, max: 128, required: true, index: true,
+  },
+  sourceType: {
+    type: String, max: 128, required: true, index: true,
+  },
+  sourceData: {},
+  artist: {
+    type: String,
+    max: 128,
+    required: true,
+    set: (artist) => artist.normalize('NFKC'),
+  },
+  title: {
+    type: String,
+    max: 128,
+    required: true,
+    set: (title) => title.normalize('NFKC'),
+  },
+  duration: { type: Number, min: 0, default: 0 },
+  thumbnail: { type: String, max: 256, default: '' },
+}, {
+  timestamps: true,
+  minimize: false,
+});
 
-  uw.mongo.model('Media', schema);
-}
-
-module.exports = mediaModel;
+module.exports = schema;
--- src/models/Playlist.js
+++ src/models/Playlist.js
@@ -2,48 +2,38 @@
 
 const mongoose = require('mongoose');
 
-/**
- * @template Element
- * @template Pagination
- * @typedef {import('../Page')<Element, Pagination>} Page
- */
-
 const { Schema } = mongoose;
 const { Types } = mongoose.Schema;
 
-async function playlistModel(uw) {
-  const schema = new Schema({
-    name: {
-      type: String,
-      min: 0,
-      max: 128,
-      required: true,
-      set: (name) => name.normalize('NFKC'),
-    },
-    description: { type: String, min: 0, max: 512 },
-    author: {
-      type: Types.ObjectId, ref: 'User', required: true, index: true,
-    },
-    shared: { type: Boolean, default: false },
-    nsfw: { type: Boolean, default: false },
-    media: [{
-      type: Types.ObjectId,
-      ref: 'PlaylistItem',
-      required: true,
-      index: true,
-    }],
-  }, {
-    collection: 'playlists',
-    timestamps: true,
-    toJSON: { getters: true },
-    minimize: false,
-  });
+const schema = new Schema({
+  name: {
+    type: String,
+    min: 0,
+    max: 128,
+    required: true,
+    set: (name) => name.normalize('NFKC'),
+  },
+  description: { type: String, min: 0, max: 512 },
+  author: {
+    type: Types.ObjectId, ref: 'User', required: true, index: true,
+  },
+  shared: { type: Boolean, default: false },
+  nsfw: { type: Boolean, default: false },
+  media: [{
+    type: Types.ObjectId,
+    ref: 'PlaylistItem',
+    required: true,
+    index: true,
+  }],
+}, {
+  collection: 'playlists',
+  timestamps: true,
+  toJSON: { getters: true },
+  minimize: false,
+});
 
-  schema.virtual('size').get(function size() {
-    return this.media.length;
-  });
+schema.virtual('size').get(function size() {
+  return this.media.length;
+});
 
-  uw.mongo.model('Playlist', schema);
-}
-
-module.exports = playlistModel;
+module.exports = schema;
