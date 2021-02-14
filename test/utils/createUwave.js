f36043d027236b4361d89c07332fb6b52f7bfe9f
Renée Kooi <renee@kooi.me>
2021-02-14 13:58:31 +0100

    Use a temporary Redis server for tests
    This prevents interfering with the "main" server on developer systems.

--- test/utils/createUwave.js
+++ test/utils/createUwave.js
@@ -1,20 +1,50 @@
 'use strict';
 
 const getPort = require('get-port');
-const Redis = require('ioredis');
+const { once } = require('events');
+const { spawn } = require('child_process');
 const deleteDatabase = require('./deleteDatabase');
 const uwave = require('../..');
 const testPlugin = require('./plugin');
 
 const DB_HOST = process.env.MONGODB_HOST || 'localhost';
 
+/**
+ * Create an in-memory redis database to run tests against.
+ */
+async function createIsolatedRedis() {
+  const port = await getPort();
+
+  const proc = spawn('redis-server', ['-']);
+  proc.stdin.end(`
+    port ${port}
+    save ""
+  `);
+
+  await once(proc, 'spawn');
+
+  for await (const buf of proc.stdout) {
+    if (buf.toString().includes('Ready to accept connections')) {
+      break;
+    }
+  }
+
+  return {
+    port,
+    proc,
+  };
+}
+
 async function createUwave(name, options) {
+  const redisServer = await createIsolatedRedis();
   const mongoUrl = `mongodb://${DB_HOST}/uw_test_${name}`;
+
   const port = await getPort();
 
   const uw = uwave({
     ...options,
     port,
+    redis: `redis://localhost:${redisServer.port}`,
     mongo: mongoUrl,
     secret: Buffer.from(`secret_${name}`),
   });
@@ -24,10 +54,8 @@ async function createUwave(name, options) {
   uw.destroy = async () => {
     await uw.close();
 
-    // Clear state between test runs.
-    const redis = new Redis('redis://localhost:6379');
-    await redis.flushall();
-    await redis.quit();
+    redisServer.proc.kill('SIGINT');
+    await once(redisServer.proc, 'close');
 
     await deleteDatabase(mongoUrl);
   };
