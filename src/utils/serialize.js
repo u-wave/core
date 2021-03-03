23330ed8b5c0e0f6f8455027cb7bcb71f8536729
Renée Kooi <renee@kooi.me>
2021-03-03 09:55:14 +0100

    support lean playlist objects in serializePlaylist()

--- src/utils/serialize.js
+++ src/utils/serialize.js
@@ -2,7 +2,7 @@
 
 function serializePlaylist(model) {
   return {
-    _id: model.id,
+    _id: model.id || model._id.toString(),
     name: model.name,
     author: model.author,
     createdAt: model.createdAt,
