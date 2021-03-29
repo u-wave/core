dff393bbafa1fcf56e717f24394b98faa339a2a7
Renée Kooi <renee@kooi.me>
2021-03-29 11:41:21 +0200

    Remove obsolete NotFoundError

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const Uwave = require('./Uwave');
 const HttpApi = require('./HttpApi');
 const SocketServer = require('./SocketServer');
 const UwaveError = require('./errors/UwaveError');
-const NotFoundError = require('./errors/NotFoundError');
 
 module.exports = function uwave(opts) {
   return new Uwave(opts);
@@ -13,7 +12,6 @@ module.exports = function uwave(opts) {
 Object.assign(module.exports, {
   Uwave,
   UwaveError,
-  NotFoundError,
   HttpApi,
   SocketServer,
 });
