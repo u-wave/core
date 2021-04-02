dee7d6efa90be8ac2e8a4bf9cf86c589d6a03c2e
Renée Kooi <renee@kooi.me>
2021-04-02 13:57:14 +0200

    try up to 5 seconds

--- test/utils/deleteDatabase.js
+++ test/utils/deleteDatabase.js
@@ -2,6 +2,9 @@
 
 const { once } = require('events');
 const mongoose = require('mongoose');
+const delay = require('delay');
+
+const IN_PROGRESS_ERROR = 12586;
 
 module.exports = async function deleteDatabase(url) {
   const defaultOptions = {
@@ -13,6 +16,20 @@ module.exports = async function deleteDatabase(url) {
 
   const mongo = mongoose.createConnection(url, defaultOptions);
   await once(mongo, 'connected');
-  await mongo.dropDatabase();
+
+  for (let i = 0; i < 50; i += 1) {
+    try {
+      await mongo.dropDatabase();
+      break;
+    } catch (error) {
+      if (error.code === IN_PROGRESS_ERROR) {
+        console.log('database op in progress...waiting');
+        await delay(100);
+      } else {
+        throw error;
+      }
+    }
+  }
+
   await mongo.close();
 };
