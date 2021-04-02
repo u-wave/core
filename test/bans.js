6c669621558b85ea95cbb81562de735b2bba5236
Renée Kooi <renee@kooi.me>
2021-04-02 14:51:17 +0200

    test /api/bans endpoint

--- test/bans.js
+++ test/bans.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const assert = require('assert');
+const supertest = require('supertest');
+const sinon = require('sinon');
 const ms = require('ms');
 const createUwave = require('./utils/createUwave');
 
@@ -41,6 +43,56 @@ describe('Bans', () => {
 
       await uw.bans.unban(user, { moderator });
       assert.strictEqual(await uw.bans.isBanned(user.id), false);
+    });
+  });
+
+  describe('GET /bans', () => {
+    it('requires authentication', async () => {
+      await supertest(uw.server)
+        .get('/api/bans')
+        .expect(401);
+    });
+
+    it('requires the users.bans.list role', async () => {
+      const token = await uw.test.createTestSessionToken(user);
+
+      await supertest(uw.server)
+        .get('/api/bans')
+        .set('Cookie', `uwsession=${token}`)
+        .expect(403);
+
+      await uw.acl.allow(user, ['users.bans.list']);
+
+      await supertest(uw.server)
+        .get('/api/bans')
+        .set('Cookie', `uwsession=${token}`)
+        .expect(200);
+    });
+
+    it('returns bans', async () => {
+      const token = await uw.test.createTestSessionToken(user);
+      await uw.acl.allow(user, ['users.bans.list', 'users.bans.add']);
+
+      const bannedUser = await uw.test.createUser();
+      await uw.bans.ban(bannedUser, {
+        moderator: user,
+        duration: ms('10 hours'),
+        reason: 'just to test',
+      });
+
+      const res = await supertest(uw.server)
+        .get('/api/bans')
+        .set('Cookie', `uwsession=${token}`)
+        .expect(200);
+
+      assert.strictEqual(res.body.meta.results, 1);
+      sinon.assert.match(res.body.data[0], {
+        duration: 36000000,
+        expiresAt: sinon.match.string,
+        reason: 'just to test',
+        moderator: user.id,
+        user: bannedUser.id,
+      });
     });
   });
 });
