45fe2775408975c37ba01a6e98d372a019840556
Renée Kooi <renee@kooi.me>
2021-04-06 11:38:04 +0200

    use typings included in secure-json-parse

--- src/secure-json-parse.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'secure-json-parse' {
-  type Options = {
-    protoAction?: 'error' | 'remove' | 'ignore',
-    constructorAction?: 'error' | 'remove' | 'ignore',
-  };
-  export function parse(text: string, reviver?: (this: any, key: string, value: any) => any, options?: Options): any;
-  export function safeParse(text: string, reviver?: (this: any, key: string, value: any) => any): any;
-  export function scan(object: any, options?: Options): void;
-}
