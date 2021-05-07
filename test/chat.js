2bd704007cdd54fa7fae42464f7dc62986f73184
Renée Kooi <renee@kooi.me>
2021-05-07 15:59:17 +0200

    reenable chat tests??

--- test/chat.js
+++ test/chat.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const assert = require('assert');
+const events = require('events');
 const sinon = require('sinon');
 const delay = require('delay');
 const createUwave = require('./utils/createUwave');
@@ -8,7 +9,7 @@ const createUwave = require('./utils/createUwave');
 const sandbox = sinon.createSandbox();
 
 // Can't get this to be reliable, skip for now
-describe.skip('Chat', () => {
+describe('Chat', () => {
   let uw;
 
   beforeEach(async () => {
@@ -24,15 +25,23 @@ describe.skip('Chat', () => {
 
     const ws = await uw.test.connectToWebSocketAs(user);
 
-    const receivedMessages = [];
-    ws.on('message', (data) => {
-      receivedMessages.push(JSON.parse(data));
-    });
-
     ws.send(JSON.stringify({ command: 'sendChat', data: 'Message text' }));
-    await delay(500);
 
-    assert(receivedMessages.some((message) => message.command === 'chatMessage' && message.data.userID === user.id && message.data.message === 'Message text'));
+    let found = false;
+    await Promise.race([
+      (async () => {
+        for await (const message of events.on(ws, 'message')) {
+          const { command, data } = JSON.parse(message);
+          if (command === 'chatMessage' && data.userID === user.id && data.message === 'Message text') {
+            found = true;
+            break;
+          }
+        }
+      })(),
+      delay(500),
+    ]);
+
+    assert(found);
   });
 
   it('does not broadcast chat messages from muted users', async () => {
@@ -54,9 +63,26 @@ describe.skip('Chat', () => {
     ws.send(JSON.stringify({ command: 'sendChat', data: 'unmuted' }));
     mutedWs.send(JSON.stringify({ command: 'sendChat', data: 'muted' }));
 
-    await delay(1500);
+    let foundUnmuted = false;
+    let foundMuted = false;
+    await Promise.race([
+      (async () => {
+        for await (const message of events.on(ws, 'message')) {
+          const { command, data } = JSON.parse(message);
+          if (command === 'chatMessage') {
+            if (data.userID === user.id) {
+              foundUnmuted = true;
+            }
+            if (data.userID === mutedUser.id) {
+              foundMuted = true;
+            }
+          }
+        }
+      })(),
+      delay(1500),
+    ]);
 
-    assert(receivedMessages.some((message) => message.command === 'chatMessage' && message.data.userID === user.id));
-    assert(!receivedMessages.some((message) => message.command === 'chatMessage' && message.data.userID === mutedUser.id));
+    assert(foundUnmuted);
+    assert(!foundMuted);
   });
 });
