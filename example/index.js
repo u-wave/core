344c8b03e7169a622ebf9d4e2c9595a5c15b6734
Uriell <uriellarttusviana+github@gmail.com>
2021-06-29 06:03:57 -0300

    beefing up example with docker(-compose) for easy single-command setup (#448)
    Co-authored-by: Renée Kooi <renee@kooi.me>

--- example/index.js
+++ example/index.js
@@ -10,7 +10,12 @@ dotenv.config();
 const port = process.env.PORT || 80;
 const secret = Buffer.from(process.env.SECRET, 'hex');
 
+const DEFAULT_MONGO_URL = 'mongodb://localhost:27017/uwave';
+const DEFAULT_REDIS_URL = 'redis://localhost:6379';
+
 const uw = uwave({
+  mongo: process.env.MONGO_CONNECTION_URL || DEFAULT_MONGO_URL,
+  redis: process.env.REDIS_CONNECTION_URL || DEFAULT_REDIS_URL,
   port,
   secret,
   // This has to be disabled so the headers do not get added to the web client too.
