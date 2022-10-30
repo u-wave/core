3f30c15050fb0b9e72116378eba2fc44f5dc11a7
Renée Kooi <renee@kooi.me>
2022-10-30 16:16:27 +0100

    implement file uploads

--- src/controllers/server.js
+++ src/controllers/server.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const { finished } = require('stream');
+const { NotFound } = require('http-errors');
+const busboy = require('busboy');
 const toItemResponse = require('../utils/toItemResponse');
 
 /**
@@ -26,7 +29,7 @@ async function getAllConfig(req) {
 }
 
 /**
- * @type {import('../types').AuthenticatedController}
+ * @type {import('../types').AuthenticatedController<{ key: string }>}
  */
 async function getConfig(req) {
   const { config } = req.uwave;
@@ -44,7 +47,7 @@ async function getConfig(req) {
 }
 
 /**
- * @type {import('../types').AuthenticatedController}
+ * @type {import('../types').AuthenticatedController<{ key: string }>}
  */
 async function updateConfig(req) {
   const { config } = req.uwave;
@@ -58,9 +61,72 @@ async function updateConfig(req) {
   });
 }
 
+function getPath(schema, path) {
+  const parts = path.split('.');
+  let descended = schema;
+  for (const part of parts) {
+    descended = descended.properties[part];
+    if (!descended) {
+      return null;
+    }
+  }
+  return descended;
+}
+
+/**
+ * @type {import('../types').AuthenticatedController<{ key: string }, {}, never>}
+ */
+async function uploadFile(req) {
+  const { config, assets } = req.uwave;
+  const { key } = req.params;
+
+  const combinedSchema = config.getSchema();
+  const schema = getPath(combinedSchema, key);
+  if (!schema) {
+    throw new NotFound('Config key does not exist');
+  }
+  if (schema.type !== 'string' || schema.format !== 'asset') {
+    throw new NotFound('Config key is not an asset');
+  }
+
+  const [content, meta] = await new Promise((resolve, reject) => {
+    const bb = busboy({ headers: req.headers });
+    bb.on('file', (name, file, info) => {
+      if (name !== 'file') {
+        return;
+      }
+
+      /** @type {Buffer[]} */
+      const chunks = [];
+      file.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+
+      finished(file, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve([Buffer.concat(chunks), info]);
+        }
+      });
+    });
+    req.pipe(bb);
+  });
+
+  const path = await assets.store(meta.filename, content, {
+    category: 'config',
+    userID: req.user._id,
+  });
+
+  return toItemResponse({ path }, {
+    url: req.fullUrl,
+  });
+}
+
 module.exports = {
   getServerTime,
   getAllConfig,
   getConfig,
   updateConfig,
+  uploadFile,
 };
