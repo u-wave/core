780c7989417769ea86c3e2237e8fd79b1b071b43
Renée <renee@kooi.me>
2023-03-08 23:50:53 +0100

    Handle emote loading failure, add twitch + ffz (#555)

--- src/plugins/emotes.js
+++ src/plugins/emotes.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { NotFound } = require('http-errors');
 const { AppTokenAuthProvider } = require('@twurple/auth');
 const { ApiClient } = require('@twurple/api');
 const fetch = require('node-fetch').default;
@@ -10,14 +11,19 @@ const schema = require('../schemas/emotes.json');
  * @typedef {{
  *   clientId: string | null,
  *   clientSecret: string | null,
+ *   useTwitchGlobalEmotes: boolean,
  *   bttv: boolean,
+ *   ffz: boolean,
  *   seventv: boolean,
  *   channels: string[],
  * }} TwitchSettings
  * @typedef {{ twitch: TwitchSettings }} EmotesSettings
  *
  * @typedef {{ id: string, code: string, imageType: string, animated: boolean }} BTTVEmote
+ * @typedef {{ id: string, name: string }} FFZEmote
+ * @typedef {{ emoticons: FFZEmote[] }} FFZEmoteSet
  * @typedef {{ id: string, name: string, data: { animated: boolean } }} SevenTVEmote
+ * @typedef {{ name: string, url: URL }} Emote
  */
 
 /**
@@ -27,108 +33,176 @@ const schema = require('../schemas/emotes.json');
  */
 async function fetchJSON(url) {
   const res = await fetch(url);
+
+  if (!res.ok) {
+    if (res.status === 404) {
+      throw new NotFound();
+    }
+    throw new Error('Unexpected response');
+  }
+
   const json = await res.json();
   return json;
 }
 
 /**
- * @param {string[]} channels
- * @returns {Promise<Record<string, URL>>}
+* @param {BTTVEmote} bttv
+* @returns {Emote}
+*/
+function fromBTTVEmote(bttv) {
+  return {
+    // The `replace` is basically just for :tf: …
+    name: bttv.code.replace(/(^:|:$)/g, ''),
+    url: new URL(`https://cdn.betterttv.net/emote/${bttv.id}/2x`),
+  };
+}
+
+async function getBTTVGlobalEmotes() {
+  /** @type {BTTVEmote[]} */
+  const emotes = await fetchJSON('https://api.betterttv.net/3/cached/emotes/global');
+  return emotes.map(fromBTTVEmote);
+}
+
+/**
+ * @param {string} channelId
+ * @returns {Promise<Emote[]>}
  */
-async function loadBTTVEmotes(channels) {
-  /** @type {Record<string, URL>} */
-  const emotes = {};
-
-  /**
-   * @param {string} channelId
-   * @returns {Promise<BTTVEmote[]>}
-   */
-  async function loadChannelEmotes(channelId) {
-    /** @type {{ channelEmotes: BTTVEmote[], sharedEmotes: BTTVEmote[] }} */
-    const channel = await fetchJSON(`https://api.betterttv.net/3/cached/users/twitch/${channelId}`);
-    const { channelEmotes, sharedEmotes } = channel;
-
-    return [...channelEmotes, ...sharedEmotes];
+async function getBTTVChannelEmotes(channelId) {
+  let channel = null;
+  try {
+    channel = /** @type {{ channelEmotes: BTTVEmote[], sharedEmotes: BTTVEmote[] }} */ (
+      await fetchJSON(`https://api.betterttv.net/3/cached/users/twitch/${channelId}`)
+    );
+  } catch (err) {
+    if (!(err instanceof NotFound)) {
+      throw err;
+    }
+  }
+  if (!channel) {
+    return [];
   }
 
-  const list = await Promise.all([
-    /** @type {Promise<BTTVEmote[]>} */ (fetchJSON('https://api.betterttv.net/3/cached/emotes/global')),
-    ...channels.map((channelId) => loadChannelEmotes(channelId)),
-  ]);
+  const { channelEmotes, sharedEmotes } = channel;
 
-  for (const emote of list.flat()) {
-    emotes[emote.code.replace(/(^:|:$)/g, '')] = new URL(`https://cdn.betterttv.net/emote/${emote.id}/2x`);
-  }
-
-  return emotes;
+  return [...channelEmotes, ...sharedEmotes].map(fromBTTVEmote);
 }
 
 /**
  * @param {string[]} channels
- * @returns {Promise<Record<string, URL>>}
+ * @returns {Promise<Emote[]>}
  */
-async function loadSevenTVEmotes(channels) {
-  /** @type {Record<string, URL>} */
-  const emotes = {};
+async function getBTTVEmotes(channels) {
+  const list = await Promise.all([
+    getBTTVGlobalEmotes(),
+    ...channels.map((channelId) => getBTTVChannelEmotes(channelId)),
+  ]);
 
-  /**
-   * @param {string} channelId
-   * @returns {Promise<SevenTVEmote[]>}
-   */
-  async function loadChannelEmotes(channelId) {
-    /** @type {{ emote_set: { emotes: SevenTVEmote[] } }} */
-    const channel = await fetchJSON(`https://7tv.io/v3/users/twitch/${channelId}`);
+  return list.flat();
+}
 
-    return channel.emote_set.emotes;
+/**
+* @param {FFZEmote} emote
+* @returns {Emote}
+*/
+function fromFFZEmote(emote) {
+  return {
+    name: emote.name,
+    url: new URL(`https://cdn.frankerfacez.com/emoticon/${emote.id}/2`),
+  };
+}
+
+/**
+ * @param {string} channelName
+ * @returns {Promise<Emote[]>}
+ */
+async function getFFZChannelEmotes(channelName) {
+  let channel = null;
+  try {
+    channel = /** @type {{ sets: Record<number, FFZEmoteSet> }} */ (
+      await fetchJSON(`https://api.frankerfacez.com/v1/room/${channelName}`)
+    );
+  } catch (err) {
+    if (!(err instanceof NotFound)) {
+      throw err;
+    }
+  }
+  if (!channel) {
+    return [];
   }
 
+  return Object.values(channel.sets)
+    .flatMap((set) => set.emoticons)
+    .map(fromFFZEmote);
+}
+
+/**
+ * @param {string[]} channels
+ * @returns {Promise<Emote[]>}
+ */
+async function getFFZEmotes(channels) {
+  const list = await Promise.all(channels.map((channelId) => getFFZChannelEmotes(channelId)));
+
+  return list.flat();
+}
+
+/**
+ * @param {SevenTVEmote} emote
+ * @returns {Emote}
+ */
+function fromSevenTVEmote(emote) {
+  const ext = emote.data.animated ? 'gif' : 'png';
+  return {
+    name: emote.name,
+    url: new URL(`https://cdn.7tv.app/emote/${emote.id}/2x.${ext}`),
+  };
+}
+
+/**
+ * @param {string} channelId
+ * @returns {Promise<Emote[]>}
+ */
+async function getSevenTVChannelEmotes(channelId) {
+  let channel = null;
+  try {
+    channel = /** @type {{ emote_set?: { emotes: SevenTVEmote[] } }} */ (
+      await fetchJSON(`https://7tv.io/v3/users/twitch/${channelId}`)
+    );
+  } catch (err) {
+    if (!(err instanceof NotFound)) {
+      throw err;
+    }
+  }
+  if (!channel || !channel.emote_set) {
+    return [];
+  }
+
+  return channel.emote_set.emotes.map(fromSevenTVEmote);
+}
+
+/**
+ * @param {string[]} channels
+ * @returns {Promise<Emote[]>}
+ */
+async function getSevenTVEmotes(channels) {
   /** @type {Promise<{ emotes: SevenTVEmote[] }>} */
   const global = fetchJSON('https://7tv.io/v3/emote-sets/global');
-  const list = await Promise.all([
-    global.then((data) => data.emotes),
-    ...channels.map((channelId) => loadChannelEmotes(channelId)),
+  const emotes = await Promise.all([
+    global.then((data) => data.emotes.map(fromSevenTVEmote)),
+    ...channels.map((channelId) => getSevenTVChannelEmotes(channelId)),
   ]);
 
-  for (const emote of list.flat()) {
-    const ext = emote.data.animated ? 'gif' : 'png';
-    emotes[emote.name] = new URL(`https://cdn.7tv.app/emote/${emote.id}/2x.${ext}`);
-  }
-
-  return emotes;
+  return emotes.flat();
 }
 
 /**
- * @param {TwitchSettings} options
- * @returns {Promise<Record<string, URL>>}
+ * @param {import('@twurple/api').HelixEmote} emote
+ * @returns {Emote}
  */
-async function loadTTVEmotes(options) {
-  /** @type {Record<string, URL>} */
-  const emotes = {};
-
-  if (!options.clientId || !options.clientSecret) {
-    return emotes;
-  }
-
-  const client = new ApiClient({
-    authProvider: new AppTokenAuthProvider(options.clientId, options.clientSecret),
-  });
-
-  const channels = /** @type {string[]} */ ((
-    await Promise.all(options.channels.map(async (channelName) => {
-      const user = await client.users.getUserByName(channelName);
-      return user?.id;
-    }))
-  ).filter((id) => id != null));
-
-  if (options.bttv) {
-    Object.assign(emotes, await loadBTTVEmotes(channels));
-  }
-
-  if (options.seventv) {
-    Object.assign(emotes, await loadSevenTVEmotes(channels));
-  }
-
-  return emotes;
+function fromTwitchEmote(emote) {
+  return {
+    name: emote.name,
+    url: new URL(emote.getImageUrl(2)),
+  };
 }
 
 /**
@@ -136,11 +210,12 @@ async function loadTTVEmotes(options) {
  *
  * Before considering this stable:
  * - error handling must be improved.
- * - we should also add support for Twitch emotes themselves.
  * - global emotes from the emote services should be optional.
  */
 class Emotes {
   #uw;
+
+  #logger;
 
   /** @type {Record<string, URL>} */
   #emotes = Object.create(null);
@@ -152,6 +227,7 @@ class Emotes {
    */
   constructor(uw) {
     this.#uw = uw;
+    this.#logger = uw.logger.child({ ns: 'uwave:emotes' });
 
     uw.config.register(schema['uw:key'], schema);
     const unsubscribe = uw.config.subscribe(
@@ -171,11 +247,72 @@ class Emotes {
     return Object.entries(this.#emotes).map(([name, url]) => ({ name, url: url.toString() }));
   }
 
+  /**
+   * @param {TwitchSettings} options
+   * @returns {Promise<Record<string, URL>>}
+   */
+  async #loadTTVEmotes(options) {
+    if (!options.clientId || !options.clientSecret) {
+      return {};
+    }
+
+    const client = new ApiClient({
+      authProvider: new AppTokenAuthProvider(options.clientId, options.clientSecret),
+    });
+
+    const channels = /** @type {string[]} */ ((
+      await Promise.all(options.channels.map(async (channelName) => {
+        const user = await client.users.getUserByName(channelName);
+        return user?.id;
+      }))
+    ).filter((id) => id != null));
+
+    const promises = channels.map(async (channelId) => {
+      const list = await client.chat.getChannelEmotes(channelId);
+      return list.map(fromTwitchEmote);
+    });
+    if (options.useTwitchGlobalEmotes) {
+      promises.push(
+        client.chat.getGlobalEmotes().then((globalEmotes) => globalEmotes.map(fromTwitchEmote)),
+      );
+    }
+
+    if (options.bttv) {
+      promises.push(getBTTVEmotes(channels));
+    }
+
+    if (options.ffz) {
+      promises.push(getFFZEmotes(options.channels));
+    }
+
+    if (options.seventv) {
+      promises.push(getSevenTVEmotes(channels));
+    }
+
+    /** @type {Record<string, URL>} */
+    const emotes = {};
+
+    const results = await Promise.allSettled(promises);
+    for (const result of results) {
+      if (result.status === 'fulfilled') {
+        for (const emote of result.value) {
+          emotes[emote.name] = emote.url;
+        }
+      } else {
+        this.#logger.warn(result.reason);
+      }
+    }
+
+    return emotes;
+  }
+
   async #reloadEmotes() {
+    this.#logger.info('reloading third-party emotes');
+
     const config = /** @type {EmotesSettings} */ (await this.#uw.config.get(schema['uw:key']));
 
     if (config.twitch) {
-      this.#emotes = await loadTTVEmotes(config.twitch);
+      this.#emotes = await this.#loadTTVEmotes(config.twitch);
     }
 
     this.#uw.publish('emotes:reload', null);
