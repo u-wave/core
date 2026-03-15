6926afae722c8be80e554912555bc4654490a09c
Renée Kooi <renee@kooi.me>
2026-03-15 14:46:42 +0100

    cleanup unused socket auth tokens

--- src/AuthRegistry.js
+++ src/AuthRegistry.js
@@ -1,17 +1,40 @@
 import { sql } from 'kysely';
 import nodeCrypto from 'node:crypto';
 import { promisify } from 'node:util';
+import { addMinutes, isAfter } from './utils/date.js';
 
 const randomBytes = promisify(nodeCrypto.randomBytes);
 
+/** @type {import('kysely').RawBuilder<Date>} */
+const earliestValidTokenTime = sql`(strftime('%FT%TZ', 'now', '-60 seconds'))`;
+
 class AuthRegistry {
   #db;
+
+  #latestCleanup = new Date();
 
   /**
    * @param {import('./schema.js').Kysely} db
    */
   constructor(db) {
     this.#db = db;
+  }
+
+  async #cleanup() {
+    await this.#db.deleteFrom('socketAuthTokens')
+      .where('createdAt', '<', earliestValidTokenTime)
+      .execute();
+  }
+
+  #autoCleanup() {
+    const now = new Date();
+    const nextCleanup = addMinutes(this.#latestCleanup, 1);
+    if (isAfter(now, nextCleanup)) {
+      this.#latestCleanup = now;
+      this.#cleanup().catch((err) => {
+        console.warn(err);
+      });
+    }
   }
 
   /**
@@ -42,14 +65,11 @@ class AuthRegistry {
 
     const result = await this.#db.deleteFrom('socketAuthTokens')
       .where('id', '=', token)
-      .where(
-        'createdAt',
-        '>',
-        /** @type {import('kysely').RawBuilder<Date>} */
-        (sql`(strftime('%FT%TZ', 'now', '-60 seconds'))`),
-      )
+      .where('createdAt', '>=', earliestValidTokenTime)
       .returning(['userID', 'sessionID'])
       .executeTakeFirstOrThrow();
+
+    this.#autoCleanup();
 
     return result;
   }
