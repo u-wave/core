u-wave/core

commit a96c1704bf1aabb7e0f989590d80967b426edb23

Switch tests to use vitest (#756)

vitest.config.js
@@ -1,0 +1,12 @@
+import { defineConfig } from 'vitest/config'; // eslint-disable-line import/no-unresolved
+
+export default defineConfig({
+  test: {
+    include: ['./test/*.{js,cjs,mjs}'],
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['lcov'],
+    },
+  },
+});

eslint.config.js
@@ -612,7 +612,7 @@ export default ts.config(
     },
   },
   {
-    files: ['eslint.config.js'],
+    files: ['eslint.config.js', 'vitest.config.js'],
     rules: {
       'n/no-unpublished-import': 'off',
       'n/no-unpublished-require': 'off',
@@ -621,7 +621,7 @@ export default ts.config(
   {
     files: ['test/**/*.js'],
     languageOptions: {
-      globals: globals.mocha,
+      globals: globals.vitest,
     },
     rules: {
       'n/no-unpublished-require': 'off',
@@ -630,7 +630,7 @@ export default ts.config(
   {
     files: ['test/**/*.mjs'],
     languageOptions: {
-      globals: globals.mocha,
+      globals: globals.vitest,
     },
     rules: {
       'n/no-unpublished-import': 'off',

test/acl.mjs
@@ -1,4 +1,7 @@
 import assert from 'assert';
+import {
+  describe, it, beforeEach, afterEach,
+} from 'vitest';
 import supertest from 'supertest';
 import * as sinon from 'sinon';
 import createUwave from './utils/createUwave.mjs';

test/authenticate.mjs
@@ -1,4 +1,7 @@
 import assert from 'assert';
+import {
+  describe, it, beforeEach, afterEach,
+} from 'vitest';
 import * as sinon from 'sinon';
 import supertest from 'supertest';
 import nock from 'nock';

test/bans.mjs
@@ -1,4 +1,7 @@
 import assert from 'assert';
+import {
+  describe, it, beforeEach, afterEach,
+} from 'vitest';
 import supertest from 'supertest';
 import * as sinon from 'sinon';
 import ms from 'ms';

test/booth.mjs
@@ -1,6 +1,9 @@
 import assert from 'node:assert';
 import { randomUUID } from 'node:crypto';
 import { setTimeout } from 'node:timers/promises';
+import {
+  describe, it, beforeEach, afterEach,
+} from 'vitest';
 import * as sinon from 'sinon';
 import supertest from 'supertest';
 import createUwave from './utils/createUwave.mjs';

test/chat.mjs
@@ -1,6 +1,9 @@
 import assert from 'node:assert';
 import { randomUUID } from 'node:crypto';
 import { setTimeout } from 'node:timers/promises';
+import {
+  describe, it, beforeEach, afterEach,
+} from 'vitest';
 import * as sinon from 'sinon';
 import supertest from 'supertest';
 import createUwave from './utils/createUwave.mjs';

test/config.mjs
@@ -1,4 +1,7 @@
 import assert from 'assert';
+import {
+  describe, it, beforeEach, afterEach,
+} from 'vitest';
 import supertest from 'supertest';
 import * as sinon from 'sinon';
 import createUwave from './utils/createUwave.mjs';

commit 3b8c3d57e7f66dc801bebb172d8fabc1633cf00a

Only check coverage on src/
Fix a regression from #756

vitest.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     include: ['./test/*.{js,cjs,mjs}'],
     coverage: {
+      include: ['./src/**/*.{js,cjs,mjs}'],
       enabled: true,
       provider: 'v8',
       reporter: ['lcov'],